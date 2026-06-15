4b37e2bd6c4f6247f3a6fdfe9baeae6c19b86561
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-06-15 20:21:45 +0600

    get json data

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const express = require('express')
 
 const dotenv = require('dotenv')
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI;
 
 
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT
 
 
 
@@ -29,12 +29,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("doctor");
+    const detailsCollection = db.collection("details")
+
+    app.get("/details", async(req, res) => {
+      const cursor = detailsCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+
+      res.send(result);
+    });
+
+    app.get("/details/:detailsId", async(req, res) => {
+      
+      const detailsId = req.params.detailsId;
+
+      const query = {_id: new ObjectId(detailsId)};
+      console.log(query);
+
+      
+
+
+      const result = await detailsCollection.findOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
