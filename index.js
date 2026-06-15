7ca9c4be345728061d9e7542ad276dd653e76fbe
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-06-15 12:49:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = 5000
+
+app.get('/', (req,res) => {
+    res.send('server is running fine')
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
