ebb8591c0f2e1963384222265e7071d7ff1c4c3d
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-08-30 15:50:52 +0600

    added

--- index.js
+++ index.js
@@ -84,10 +84,14 @@ const verifyToken = async (req, res, next) => {
     });
   }
 };
-async function run() {
-  try {
+//async function run() {
+  //try {
     // Connect the client to the server	(optional starting in v4.7)
    // await client.connect();
+
+   client.connect(() => {
+    console.log('connecting to mongo db')
+   }).catch(console.dir)
 
     const db = client.db("doctor");
     const detailsCollection = db.collection("details");
@@ -299,13 +303,13 @@ app.delete("/booking/:id", verifyToken, async (req, res) => {
 
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+   // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+ // } finally {
     // Ensures that the client will close when you finish/error
    // await client.close();
-  }
-}
-run().catch(console.dir);
+ // }
+//}
+//run().catch(console.dir);
 
 app.get('/', (req,res) => {
     res.send('server is running fine')
